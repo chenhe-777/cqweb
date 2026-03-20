3f473e0567434a8fc675191d36a3e8aeff5080f9
chenhe-777 <chenhe777@example.com>
2026-03-20 18:16:02 +0800

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '/cqweb',
+  assetPrefix: '/cqweb/',
   images: {
     unoptimized: true,
+    domains: ['images.unsplash.com', 's3.us-west-2.amazonaws.com'],
   },
 }
 
